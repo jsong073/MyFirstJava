1b431b95fbc1a617abf1ccaf9faf05fad1d8ebfb
jsong073 <jaehasong073@gmail.com>
2020-04-13 14:59:17 -0400

    first edit

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -3,7 +3,7 @@ This is the first Java program which will print out a message, Hello world.
  */
 public class MyFirstJava {
     public static void main (String[] args) {
-        System.out.println("Hello world.");
+        System.out.println("Hello Jaeha");
 
 
     }
